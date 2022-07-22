37b20300dc6d56f7194a1f1558a9445ed87a5174
Elyor941004 <ernazarovelyor941004@gmail.com>
2022-07-22 10:45:36 +0500

    4-work

--- PdoRequest.php
+++ PdoRequest.php
@@ -2,30 +2,37 @@
 	include 'db.php';
 	$url = $_GET['cut_link'];
 	$bool = false;
-	if (strlen($_GET['cut_link'])!=null) {
-		$token = token();
-		$sql = "INSERT INTO links(link, token)VALUES('".$url."', '".$token."')";
-		if (isset($sql)) {
-			$conn->exec($sql);
-			$bool = true;
-		}
-		if($bool){
-			$_GET['cut_link'] = $_SERVER['SERVER_NAME'].'/'.$token.'/';
-		}
-
-	}
-	else{
-		$url = $_SERVER['REQUEST_URI'];
-		$substr = substr($url, 1);
-		$token = substr_replace($substr, "", -1);
-		if (strlen($token) > 0) {
-			$sql = "SELECT * FROM links WHERE token = '".$token."'";
-			$all = $conn->query($sql);
-			foreach ($all as $one){
-				header("Location: ".$one['link']);
+		if (strlen($_GET['cut_link'])!=null) {
+			$token = token();
+			$sql = "INSERT INTO links(link, token)VALUES('".$url."', '".$token."')";
+			if (isset($sql)) {
+				$conn->exec($sql);
+				$bool = true;
 			}
+			if($bool){
+				$_GET['cut_link'] = $_SERVER['SERVER_NAME'].'/'.$token.'/';
+			}
+
 		}
-	}
+		else{
+			$url = $_SERVER['REQUEST_URI'];
+			$substr = substr($url, 1);
+			$token = substr_replace($substr, "", -1);
+			if (strlen($token) > 0) {
+				$sql = "SELECT * FROM links WHERE token = '".$token."'";
+				$all = $conn->query($sql);
+				foreach ($all as $one){
+					header("Location: ".$one['link']);
+				}
+			}else{
+				if ($_GET['cut_link']=null) {
+					echo "<div class='alert alert-warning'>
+					  <strong>Warning!</strong> you haven't filled the input
+					</div>";
+				}
+			}
+				
+		}
 
 	function token(){
 		$elements = "absdefjhklmnopqstuvwxyzABSDEFJHKLMNOPQSTUVWXYZ0123456789";
--- index.php
+++ index.php
@@ -10,13 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cut Link</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <style>
-        a {
-            text-decoration: none;
-            color: black
-        }
-    </style>
+    <link rel="stylesheet" href="bootstrap.min.css">
+    
 </head>
 <body>
     <!-- <script>
@@ -31,11 +26,30 @@
         <div class="row">
             <form action="" method="GET">
                 <div class="input-group mb-3">
-                    <input type="text" name="cut_link" value="<?=$_GET['cut_link']?>" class="form-control" placeholder="Вставьте ссылку" aria-describedby="button-addon2">
-                    <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Выполнить</button>
+                    <input type="text" name="cut_link" id="cut_link" value="<?=$_GET['cut_link']?>" class="form-control" placeholder="Вставьте ссылку" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary"  type="submit" id="button-addon2">Выполнить</button>
                 </div>
             </form>
         </div>
     </div>
 </body>
+
 </html>
+<script src="jquery.3.6.0.js"></script>
+<script>
+        $(document).ready(function(){
+          $("#button-addon2").click(function(){
+            var cut_link = document.getElementById('cut_link').value;
+            if (cut_link.length == 0) {
+                alert("Warning! You haven\'nt filled the input");
+            }
+          });
+        });
+</script>
+<style>
+        a{
+            text-decoration: none;
+            color: black
+        }
+      
+    </style>
